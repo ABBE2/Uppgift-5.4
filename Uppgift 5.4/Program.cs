a792f7014ccb52e16a6dc43c8f575f60c92c8cf9
albin <albin@DESKTOP-QPCBJM5>
2023-04-14 19:09:30 +0200

    Add project files.

---
+++ Uppgift 5.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace uppgift5._4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = new string[5];
+            namn[0] = "Albin";
+            namn[1] = "Theodor";
+            namn[2] = "Melker";
+            namn[3] = "Anton";
+            namn[4] = "Felix";
+
+            for (int i = 0; i < namn.Length; i++)
+            {
+                Array.Sort(namn);
+
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
